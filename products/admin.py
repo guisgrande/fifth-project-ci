fde217127341426d285da4c17c033072719c8bd8
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-09-20 08:19:52 +0000

    Add ModelAdmin for Category and Product models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_sku', 'system_name', 'display_name')
+    search_fields = ('system_name', 'display_name')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'release_date',
+        'category',
+        'price',
+        'rating'
+        )
+    list_filter = ('release_date', 'category')
+    search_fields = ('sku', 'name')
