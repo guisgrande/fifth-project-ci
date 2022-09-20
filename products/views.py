0fd3ad177995295a6cc794b06e3e180619e7a0f9
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-09-20 15:08:53 +0000

    Import Product from models to create a list of products

--- products/views.py
+++ products/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import Product
+
 
 def products(request):
     """ A view to return the All products page """
+    products_list = Product.objects.all()
+    total_products_list = products_list.count()
 
-    return render(request, 'products_all.html')
+    context = {
+        "products_list": products_list,
+        "total_products_list": total_products_list,
+    }
+
+    return render(request, 'products_all.html', context)
