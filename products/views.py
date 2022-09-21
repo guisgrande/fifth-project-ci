683b4d61d110a8b4e2f2a9fecd1f7fb75b097912
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-09-21 09:30:58 +0000

    Add product_details function, to show individual product details

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def products(request):
     """ A view to return the All products page """
+
     products_list = Product.objects.all()
     total_products_list = products_list.count()
 
@@ -13,3 +14,15 @@ def products(request):
     }
 
     return render(request, 'products_all.html', context)
+
+
+def product_details(request, slug):
+    """ A view to show individual product details and category """
+
+    product = get_object_or_404(Product, slug=slug)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'product_details.html', context)
