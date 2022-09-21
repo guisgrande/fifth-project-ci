a998a5788ba58b95d5d3faf7864bcc02b295d6fc
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-09-21 12:07:31 +0000

    Add Allauth setting instalation setup

--- streetcraft/settings.py
+++ streetcraft/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'home',
     'products',
 ]
@@ -71,6 +75,16 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
 
 WSGI_APPLICATION = 'streetcraft.wsgi.application'
 
