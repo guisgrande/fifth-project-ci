79ff97544201dcff8c4ff3a9c9767594c2a96ce8
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-09-21 09:31:53 +0000

    Add path to product_details function at urlpatterns

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('all-products/', views.products, name='products')
+    path('all-products/', views.products, name='products'),
+    path('<slug>/', views.product_details, name='product_details')
 ]
