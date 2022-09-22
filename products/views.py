656e776f242c4d2de3e11d080694e1d9a5e1bf5b
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-09-22 12:12:09 +0000

    Add search for category at products function

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import Product
+from .models import Product, Category
 from django.db.models import Q
 from django.contrib import messages
 
@@ -7,12 +7,20 @@ from django.contrib import messages
 def products(request):
     """
     A view to return the All products page,
-    Search products funcionality
+    Search products funcionality, category return.
     """
 
     products_list = Product.objects.all()
     total_products_list = products_list.count()
     query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products_list = products_list.filter(category__system_name__in=categories)
+            categories = Category.objects.filter(system_name__in=categories)
+            total_products_list = products_list.count()
 
     if request.GET:
         if 'q' in request.GET:
@@ -23,10 +31,12 @@ def products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products_list = products_list.filter(queries)
+            total_products_list = products_list.count()
 
     context = {
         "products_list": products_list,
         "total_products_list": total_products_list,
+        'current_categories': categories,
     }
 
     return render(request, 'products_all.html', context)
