11c0636761c372fd6448b19c726527625f276179
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-09-22 10:28:49 +0000

    Add request for query, to return user search

--- products/views.py
+++ products/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product
+from django.db.models import Q
+from django.contrib import messages
 
 
 def products(request):
-    """ A view to return the All products page """
+    """
+    A view to return the All products page,
+    Search products funcionality
+    """
 
     products_list = Product.objects.all()
     total_products_list = products_list.count()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products_list = products_list.filter(queries)
 
     context = {
         "products_list": products_list,
