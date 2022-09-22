0c39c6bc0115e6608f6859b2e65e8ca70105511d
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-09-22 15:51:11 +0000

    Add shopbag to context_processors, add delivery setup values

--- streetcraft/settings.py
+++ streetcraft/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shopbag.contexts.bag_contents',
             ],
         },
     },
@@ -152,6 +153,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Delivery setup
+
+FREE_DELIVERY_THRESHOLD = 100
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
