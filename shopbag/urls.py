04fad062e7ea8f090d819d96aa921bb2c6f80675
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-09-22 16:41:07 +0000

    Add path to add_to_bag function view

--- shopbag/urls.py
+++ shopbag/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bag/', views.view_bag, name='bag')
+    path('bag/', views.view_bag, name='bag'),
+    path('add/<slug>/', views.add_to_bag, name='add_to_bag'),
 ]
