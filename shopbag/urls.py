772856a70c29e1ca567c310c517332a61920e8dd
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-09-26 15:52:00 +0000

    Add ult path for update_bar and remove_from_bag functions views

--- shopbag/urls.py
+++ shopbag/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('bag/', views.view_bag, name='bag'),
     path('add/<slug>/', views.add_to_bag, name='add_to_bag'),
+    path('update/<slug>/', views.update_bag, name='update_bag'),
+    path('remove/<slug>/', views.remove_from_bag, name='remove_from_bag'),
 ]
