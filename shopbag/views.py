f26fd28cb6fdfd38dc617eb148142f1fa60e1ee7
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-09-26 15:50:41 +0000

    Add function view for update product quantity and remove from the bag

--- shopbag/views.py
+++ shopbag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_bag(request):
@@ -26,3 +26,35 @@ def add_to_bag(request, slug):
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
+
+
+def update_bag(request, slug):
+    """
+    Update the quantity of the product to the new amount
+    """
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[slug] = quantity
+    else:
+        bag.pop(slug)
+
+    request.session['bag'] = bag
+    return redirect(reverse('bag'))
+
+
+def remove_from_bag(request, slug):
+    """
+    Remove the product from the shopping bag
+    """
+
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(slug)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
