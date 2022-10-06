e53d605226dee86ef94036a8f1fe98edd505fcf7
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-06 14:07:19 +0000

    Add stripe public key and client secret, into context

--- checkout/views.py
+++ checkout/views.py
@@ -17,6 +17,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LpsboL1ee1CsQAinhbqvK4fRfBSxM7mWqYYouhPQ1hAhp6i8OMTtltvCS7xxVIzeca7qP9fqKOH9m2QQeTQiR7d00RFaAHhus',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
