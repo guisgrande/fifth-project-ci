09a08bec6f265a4797d91f0ea902a5793b300371
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-07 10:20:31 +0000

    Add path to checkout_success function view

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('order/', views.checkout, name='checkout'),
+    path('order_success/<order_number>', views.checkout_success, name='checkout_success'),
 ]
