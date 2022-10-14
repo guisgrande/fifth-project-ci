40e5df802d2503dc786a60ed3c65ab071242499c
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-14 16:05:01 +0000

    Add new Offer class model, edit Product class

--- products/models.py
+++ products/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.utils.text import slugify
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Category(models.Model):
+    """
+    Class to represent Category database model
+    """
     category_sku = models.SmallAutoField(primary_key=True)
     system_name = models.CharField(max_length=254,)
     display_name = models.CharField(max_length=254, null=True, blank=True)
@@ -12,6 +16,29 @@ class Category(models.Model):
 
     def get_display_name(self):
         return self.display_name
+
+
+class Offer(models.Model):
+    """
+    Class to represent Offer database model
+    """
+    offer_sku = models.SmallAutoField(primary_key=True)
+    offer_name = models.CharField(max_length=50,)
+    offer_display = models.CharField(max_length=254, null=True, blank=True)
+    offer_discount = models.IntegerField(default=0,
+        validators=[
+            MaxValueValidator(80),
+            MinValueValidator(0)
+        ])
+
+    def __str__(self):
+        return str(self.offer_sku)
+   
+    def get_offer_name(self):
+        return self.offer_name
+
+    def get_offer_discount(self):
+        return self.offer_discount
 
 
 class Product(models.Model):
@@ -24,6 +51,8 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    offer_active = models.BooleanField(default=False)
+    offer = models.ForeignKey(Offer, null=True, blank=True, on_delete=models.SET_NULL)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     product_image = models.ImageField(null=True, blank=True)
     product_image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -44,3 +73,6 @@ class Product(models.Model):
         """
         self.slug = slugify(self.name)
         super(Product, self).save(*args, **kwargs)
+
+    def cal_discount_price(self):
+        return self.price - ((self.price * self.offer.offer_discount) / 100)
