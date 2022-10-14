1386feae9b672cc84186a9372fdadd75e92e80a5
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-14 16:06:02 +0000

    Add Offer model admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Offer
 
 
 @admin.register(Category)
@@ -7,6 +7,10 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('category_sku', 'system_name', 'display_name')
     search_fields = ('system_name', 'display_name')
 
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('offer_sku', 'offer_name', 'offer_display', 'offer_discount')
+    search_fields = ('offer_name', 'offer_display')
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
