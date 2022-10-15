67806dcf376f4809738f791b382818f1822e32d6
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-15 12:55:18 +0000

    Edit checkout class model, add user profile field to Order class

--- checkout/models.py
+++ checkout/models.py
@@ -5,6 +5,7 @@ from django.db.models import Sum
 from django.conf import settings
 from products.models import Product
 from django_countries.fields import CountryField
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
@@ -15,6 +16,8 @@ class Order(models.Model):
     Oder total cost, delivery cost and final total
     """
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
