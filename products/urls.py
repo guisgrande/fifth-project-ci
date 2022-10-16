8adc09f9ade3ff8097e54338cef42f4fccf69c98
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-16 14:54:21 +0000

    Add path to edit_product view

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('all-products/', views.products, name='products'),
     path('details/<slug>/', views.product_details, name='product_details'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<slug>/', views.edit_product, name='edit_product'),
 ]
