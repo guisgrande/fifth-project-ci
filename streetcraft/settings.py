67e367958c6bbb09ca5512292110165bc17ab8d2
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-16 13:27:39 +0000

    Fix broken media load in template, add context_processors to TEMPLATES

--- streetcraft/settings.py
+++ streetcraft/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'shopbag.contexts.bag_contents',
             ],
             'builtins': [
