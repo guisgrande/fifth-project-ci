0d78b6ee4e355c978cc6065f27ec7b9cd887b2d7
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-16 13:17:20 +0000

    Add new path to add_product view

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('all-products/', views.products, name='products'),
-    path('<slug>/', views.product_details, name='product_details')
+    path('details/<slug>/', views.product_details, name='product_details'),
+    path('add/', views.add_product, name='add_product'),
 ]
