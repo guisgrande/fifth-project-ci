2e875366aadb5cfceef6d5baff1f35ea903f905c
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-16 13:16:11 +0000

    Add new view function to add_product

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from .models import Product, Category, Offer
 from django.db.models import Q
 from django.contrib import messages
 from django.db.models.functions import Lower
+from .forms import ProductForm
 
 
 def products(request):
@@ -71,7 +72,9 @@ def products(request):
 
 
 def product_details(request, slug):
-    """ A view to show individual product details and category """
+    """
+    A view to show individual product details and category
+    """
 
     product = get_object_or_404(Product, slug=slug)
 
@@ -80,3 +83,28 @@ def product_details(request, slug):
     }
 
     return render(request, 'products/product_details.html', context)
+
+
+def add_product(request):
+    """
+    A view to admin add new products to store database
+    Using ProductForm
+    """
+    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
