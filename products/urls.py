22c7a41ac6b4f3f45b0f581b5952eae864121605
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-16 15:34:20 +0000

    Add path to delete product view

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('details/<slug>/', views.product_details, name='product_details'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<slug>/', views.edit_product, name='edit_product'),
+    path('delete/<slug>/', views.delete_product, name='delete_product'),
 ]
