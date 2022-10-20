8c9bb375404629b31659344cb1aebeaaee7ec1ad
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-20 11:09:25 +0000

    Add delete_profile view function, to delete user

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import UserProfile
-from .forms import UserProfileForm
+from .forms import UserProfileForm, UserDeleteForm
 
 
 @login_required
@@ -27,3 +27,25 @@ def profile(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def delete_profile(request):
+    """
+    Function for delete profile/user, permanently.
+    """
+
+    if request.method == 'POST':
+        delete_form = UserDeleteForm(request.POST, instance=request.user)
+        user = request.user
+        user.delete()
+        messages.info(request, 'Your account has been deleted.')
+        return redirect('home')
+    else:
+        delete_form = UserDeleteForm(instance=request.user)
+
+    context = {
+        'delete_form': delete_form,
+    }
+
+    return render(request, 'profiles/delete_profile.html', context)
