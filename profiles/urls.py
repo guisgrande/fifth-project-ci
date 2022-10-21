daff5d21fa1f435c7ad3ac3e527ca1f6ff019298
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-21 11:58:07 +0000

    Add path to OrderDetails view

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('user/', views.profile, name='profile'),
     path('delete-profile/', views.delete_profile, name='delete_profile'),
     path('my-orders/', views.orders_list, name='orders_list'),
+    path('order/<order_number>', views.OrderDetails.as_view(), name='order_details'),
 ]
