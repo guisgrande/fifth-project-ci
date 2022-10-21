c1021708e476328c2c2b30bef4a46a3b4b72e9e5
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-21 10:21:14 +0000

    Add orders_list view function, to return user orders

--- profiles/views.py
+++ profiles/views.py
@@ -52,3 +52,20 @@ def delete_profile(request):
     }
 
     return render(request, 'profiles/delete_profile.html', context)
+
+
+@login_required
+def orders_list(request):
+    """
+    Function for display list of all order of the user.
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
+    total_orders_list = orders.count()
+
+    context = {
+        'orders': orders,
+        'total_orders_list': total_orders_list
+    }
+
+    return render(request, 'profiles/orders_list.html', context)
