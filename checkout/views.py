9727566b5e02322d99f110a78496eaa4be429e5e
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-26 11:26:23 +0000

    Add content to checkout_success message

--- checkout/views.py
+++ checkout/views.py
@@ -161,7 +161,8 @@ def checkout_success(request, order_number):
             user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}.')
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
 
     if 'bag' in request.session:
         del request.session['bag']
