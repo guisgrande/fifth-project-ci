2b57bb9a89e0408ebb3bafa6bdd920b39af0808f
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-27 20:52:53 +0000

    Add new view to user subscribe to news letter list

--- management/views.py
+++ management/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from .models import NewsLetterList
 
-# Create your views here.
+
+def news_letter_sub(request):
+    """
+    A view to subscribe to news letter list
+    """
+    if request.method == 'POST':
+        name = request.POST.get('news_name', None)
+        email = request.POST.get('news_email', None)
+
+        if not name or not email:
+            messages.error(request, "You must type legit name and email to subscribe to a Newsletter")
+            return redirect("/")
+
+        if get_user_model().objects.filter(email=email).first():
+            messages.error(request, f"Found registered user with associated {email} email. You must login to subscribe or unsubscribe.")
+            return redirect(request.META.get("HTTP_REFERER", "/")) 
+
+        subscribe_user = NewsLetterList.objects.filter(news_email=email).first()
+        if subscribe_user:
+            messages.error(request, f"{email} email address is already subscriber.")
+            return redirect(request.META.get("HTTP_REFERER", "/"))  
+
+        try:
+            validate_email(email)
+        except ValidationError as e:
+            messages.error(request, e.messages[0])
+            return redirect("/")
+
+        subscribe_model_instance = NewsLetterList()
+        subscribe_model_instance.news_name = name
+        subscribe_model_instance.news_email = email
+        subscribe_model_instance.save()
+        messages.success(request, f'{email} email was successfully subscribed to our newsletter!')
+        return redirect(request.META.get("HTTP_REFERER", "/"))
