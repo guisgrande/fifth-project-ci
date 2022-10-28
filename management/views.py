fd07fc641fd01bc5d962b57e7ff5eb3e96178c12
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-28 20:08:31 +0000

    Edit news_letter_sub view to send email to confirm subscrition, condition to verify logged user

--- management/views.py
+++ management/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
@@ -8,7 +11,7 @@ from .models import NewsLetterList
 
 def news_letter_sub(request):
     """
-    A view to subscribe to news letter list
+    A view to user subscribe to news letter list
     """
     if request.method == 'POST':
         name = request.POST.get('news_name', None)
@@ -19,13 +22,18 @@ def news_letter_sub(request):
             return redirect("/")
 
         if get_user_model().objects.filter(email=email).first():
-            messages.error(request, f"Found registered user with associated {email} email. You must login to subscribe or unsubscribe.")
-            return redirect(request.META.get("HTTP_REFERER", "/")) 
+            if request.user.is_authenticated:
+                if email == request.user.email:
+                    pass
+            else:
+                messages.error(request, f"Found registered user with associated {email} email. Login first.")
+                return redirect(request.META.get("HTTP_REFERER", "/"))
 
         subscribe_user = NewsLetterList.objects.filter(news_email=email).first()
+        
         if subscribe_user:
-            messages.error(request, f"{email} email address is already subscriber.")
-            return redirect(request.META.get("HTTP_REFERER", "/"))  
+            messages.error(request, f"{email} email address is already subscribed.")
+            return redirect(request.META.get("HTTP_REFERER", "/"))
 
         try:
             validate_email(email)
@@ -37,5 +45,18 @@ def news_letter_sub(request):
         subscribe_model_instance.news_name = name
         subscribe_model_instance.news_email = email
         subscribe_model_instance.save()
-        messages.success(request, f'{email} email was successfully subscribed to our newsletter!')
+
+        subject = str('Street Craft - News Letter Subscrition')
+        body = render_to_string(
+            'management/confirmation_emails/confirmation_news_letter_sub.txt',
+            {'name': name, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [email]
+        )      
+
+        messages.success(request, f'{email} email was successfully subscribed to our News Letter!')
         return redirect(request.META.get("HTTP_REFERER", "/"))
