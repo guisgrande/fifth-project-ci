a5f5fdb183b61d780bd215052749ee9c6f3811b1
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-29 13:52:38 +0000

    Add path to news_letter_send and news_letter_view views

--- management/urls.py
+++ management/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('news-letter-subscribe/', views.news_letter_sub, name='news_letter_sub'),
+    path('news-letter-mails/', views.news_letter_view, name='news_letter_view'),
+    path('news-letter-send/', views.news_letter_send, name='news_letter_send'),
 ]
