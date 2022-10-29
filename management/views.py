3d2d4a4aa4418a75ff0dc66b119cad9da798f8cd
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-29 13:51:10 +0000

    Add two new views to display news letter form and send to email list

--- management/views.py
+++ management/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, reverse, render
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
@@ -6,7 +7,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
-from .models import NewsLetterList
+from .models import NewsLetterList, NewsLetterMail
 
 
 def news_letter_sub(request):
@@ -56,7 +57,57 @@ def news_letter_sub(request):
             body,
             settings.DEFAULT_FROM_EMAIL,
             [email]
-        )      
+        )
 
-        messages.success(request, f'{email} email was successfully subscribed to our News Letter!')
+        messages.success(request, f'{email} was successfully subscribed to our News Letter!')
+        return redirect(request.META.get("HTTP_REFERER", "/"))
+
+
+@login_required
+def news_letter_view(request):
+    """
+    A view to display News Letter email form.
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can view this page.')
+        return redirect(reverse('home'))
+
+    return render(request, 'management/admin_news_letter.html')
+
+
+@login_required
+def news_letter_send(request):
+    """
+    A view to site owner send News Letter email to subscribers
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, just site owner can send emails.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        subject = request.POST.get('subject', None)
+        body = request.POST.get('body', None)
+       
+        if not subject or not body:
+            messages.error(request, "You forgot to fill in one of the fields.")
+            return redirect("/")
+        
+        email_list = []
+        for subscriber in NewsLetterList.objects.all():
+            email_list.append(subscriber.news_email)
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [email_list]
+        )
+
+        news_letter_mails = NewsLetterMail()
+        news_letter_mails.subject = subject
+        news_letter_mails.body = body
+        news_letter_mails.status = 1
+        news_letter_mails.save()
+
+        messages.success(request, 'News Letter successfully sent to mailing list!')
         return redirect(request.META.get("HTTP_REFERER", "/"))
