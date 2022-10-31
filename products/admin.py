23a9e56f15d77e9c8997ff3bb5e70f3ddc7f753c
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-31 15:49:43 +0000

    Add admin model to ReviewProduct

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Offer
+from .models import Product, Category, Offer, ReviewProduct
 
 
 @admin.register(Category)
@@ -7,10 +7,12 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('category_sku', 'system_name', 'display_name')
     search_fields = ('system_name', 'display_name')
 
+
 @admin.register(Offer)
 class OfferAdmin(admin.ModelAdmin):
     list_display = ('offer_sku', 'offer_name', 'offer_display', 'offer_discount')
     search_fields = ('offer_name', 'offer_display')
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -19,8 +21,13 @@ class ProductAdmin(admin.ModelAdmin):
         'name',
         'release_date',
         'category',
-        'price',
-        'rating'
+        'price'
         )
     list_filter = ('release_date', 'category')
     search_fields = ('sku', 'name')
+
+
+@admin.register(ReviewProduct)
+class ReviewProductAdmin(admin.ModelAdmin):
+    list_display = ('product', 'name', 'created_on', 'reviewed', 'review')
+    search_fields = ('product',)
