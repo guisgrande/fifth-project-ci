98e8b2a40ffd0638f43a445f2e63eb6492be59a0
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-10-31 15:48:29 +0000

    Add new model to ReviewProduct

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -33,7 +34,7 @@ class Offer(models.Model):
 
     def __str__(self):
         return str(self.offer_sku)
-   
+
     def get_offer_name(self):
         return self.offer_name
 
@@ -56,7 +57,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     offer_active = models.BooleanField(default=False)
     offer = models.ForeignKey(Offer, null=True, blank=True, on_delete=models.SET_NULL)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     product_image = models.ImageField(null=True, blank=True)
     product_image_url = models.URLField(max_length=1024, null=True, blank=True)
     slug = models.SlugField(max_length=254, null=True, blank=True, unique=True)
@@ -66,10 +66,10 @@ class Product(models.Model):
         Class to show newest product first
         """
         ordering = ['-release_date']
-    
+
     def __str__(self):
         return str(self.sku)
-    
+
     def save(self, *args, **kwargs):
         """
         Function to auto generate slugfield
@@ -79,3 +79,36 @@ class Product(models.Model):
 
     def cal_discount_price(self):
         return self.price - ((self.price * self.offer.offer_discount) / 100)
+
+
+class ReviewProduct(models.Model):
+    """
+    Class model to review a product.
+    """
+    REVIEW_VALUE = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="product_review"
+        )
+    name = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="product_review"
+        )
+    created_on = models.DateTimeField(auto_now_add=True)
+    reviewed = models.BooleanField(default=False)
+    review = models.IntegerField(choices=REVIEW_VALUE, default=5)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return str(self.product)
