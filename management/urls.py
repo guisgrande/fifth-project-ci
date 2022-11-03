1903ce1157ed48e15b0d90c277f8c00651e664e8
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-11-03 17:42:22 +0000

    Add path to stock_control and update_stock views

--- management/urls.py
+++ management/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('news-letter-subscribe/', views.news_letter_sub, name='news_letter_sub'),
     path('news-letter-mails/', views.news_letter_view, name='news_letter_view'),
     path('news-letter-send/', views.news_letter_send, name='news_letter_send'),
+    path('stock-control/', views.stock_control, name='stock_control'),
+    path('update-stock/<slug>/', views.update_stock, name='update_stock'),
 ]
