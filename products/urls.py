08a9c6ef532c0e0bb635942110317f2f9431e632
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-11-04 10:50:28 +0000

    Move path url for add, edit and delete products, from products app to management

--- products/urls.py
+++ products/urls.py
@@ -4,9 +4,6 @@ from . import views
 urlpatterns = [
     path('all-products/', views.products, name='products'),
     path('details/<slug>/', views.product_details, name='product_details'),
-    path('add/', views.add_product, name='add_product'),
-    path('edit/<slug>/', views.edit_product, name='edit_product'),
-    path('delete/<slug>/', views.delete_product, name='delete_product'),
     path('review-product/<slug>/', views.review_product, name='review_product'),
     path('add-review/<slug>/', views.add_review, name='add_review'),
 ]
