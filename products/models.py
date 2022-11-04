aa22271eaf190869ddc8dc271abdeb4cdaae40c8
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-11-04 10:23:00 +0000

    Add MinValueValidator to Product model

--- products/models.py
+++ products/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
     display_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.system_name
+        return self.display_name
 
     def get_display_name(self):
         return self.display_name
@@ -33,7 +33,7 @@ class Offer(models.Model):
         ])
 
     def __str__(self):
-        return str(self.offer_sku)
+        return str(self.offer_display)
 
     def get_offer_name(self):
         return self.offer_name
@@ -53,7 +53,10 @@ class Product(models.Model):
     name = models.CharField(max_length=150, unique=True)
     release_date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    quantity_available = models.IntegerField(default=100)
+    quantity_available = models.IntegerField(default=100,
+            validators=[
+            MinValueValidator(0)
+        ])
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     offer = models.ForeignKey(Offer, null=True, blank=True, on_delete=models.SET_NULL)
