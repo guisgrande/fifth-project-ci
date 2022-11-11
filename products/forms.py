2fdbe15db8e7919659ff3be602afc25cae9bc66a
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-11-11 20:21:23 +0000

    Edit style for add, edit and delete products templates

--- products/forms.py
+++ products/forms.py
@@ -22,7 +22,7 @@ class ProductForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs['class'] = 'border rounded m-1 shadow-sm'
 
 
 class ReviewForm(forms.ModelForm):
