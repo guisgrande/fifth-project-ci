d8806af3c1880beca649ca349b2cca0d5706608c
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-11-15 14:20:21 +0000

    Edit index view to limit list to 4 products each

--- home/views.py
+++ home/views.py
@@ -8,11 +8,11 @@ def index(request):
     """
 
     # List for Special Sale (Offer SKU 2)
-    special_list = Product.objects.filter(offer=2).order_by("-release_date")
+    special_list = Product.objects.filter(offer=2).order_by("-release_date")[0:4]
     # List for New Arrivals (Offer SKU 1)
-    news_list = Product.objects.filter(offer=1).order_by("-release_date")
+    news_list = Product.objects.filter(offer=1).order_by("-release_date")[0:4]
     # List for Last Oportinity (Offer SKU 2)
-    last_list = Product.objects.filter(offer=3).order_by("-release_date")
+    last_list = Product.objects.filter(offer=3).order_by("-release_date")[0:4]
 
     context = {
         "special_list": special_list,
